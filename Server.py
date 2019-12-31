737b9e6fcd8f1d5f36f0589dd09bbc3075685d0c
coolshou <coolshou2060@gmail.com>
2019-12-31 14:21:19 +0800

    fix format from tab to 4 space

--- Server.py
+++ Server.py
@@ -5,26 +5,26 @@ from ServerWorker import ServerWorker
 
 class Server:
 
-	def main(self):
-		try:
-			SERVER_PORT = int(sys.argv[1])
-		except:
-			print "[Usage: Server.py Server_port]\n"
-		rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		rtspSocket.bind(('', SERVER_PORT))
-		print "RTSP Listing incoming request..."
-		rtspSocket.listen(5)
+    def main(self):
+        try:
+            SERVER_PORT = int(sys.argv[1])
+        except:
+            print("[Usage: Server.py Server_port]\n")
+        rtspSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        rtspSocket.bind(('', SERVER_PORT))
+        print("RTSP Listing incoming request...")
+        rtspSocket.listen(5)
 
-		# Receive client info (address,port) through RTSP/TCP session
-		while True:
-			clientInfo = {}
-			clientInfo['rtspSocket'] = rtspSocket.accept()   # this accept {SockID,tuple object},tuple object = {clinet_addr,intNum}!!!
+        # Receive client info (address,port) through RTSP/TCP session
+        while True:
+            clientInfo = {}
+            clientInfo['rtspSocket'] = rtspSocket.accept()   # this accept {SockID,tuple object},tuple object = {clinet_addr,intNum}!!!
 
-			ServerWorker(clientInfo).run()
+            ServerWorker(clientInfo).run()
 
 
 # Program Start Point
 if __name__ == "__main__":
-	(Server()).main()
+    (Server()).main()
 
 
