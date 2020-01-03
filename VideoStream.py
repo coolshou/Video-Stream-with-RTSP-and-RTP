ae6d7ae2cc0584ded616026c1e745944a008eb5e
coolshou <coolshou2060@gmail.com>
2020-01-03 13:41:43 +0800

    fix bytearray index out of range

--- VideoStream.py
+++ VideoStream.py
@@ -19,27 +19,25 @@ class VideoStream:
         data = self.file.read(5) # Get the framelength from the first 5 bytes
         #data_ints = struct.unpack('<' + 'B'*len(data),data)
         data = bytearray(data)
-
-        data_int = (data[0] - 48) * 10000 + (data[1] - 48) * 1000 + (data[2] - 48) * 100 + (data[3] - 48) * 10 + (data[4] - 48)# = #int(data.encode('hex'),16)
-
-        final_data_int = data_int
-
         if data:
+            if len(data)>=5:
+                data_int = (data[0] - 48) * 10000 + (data[1] - 48) * 1000 + (data[2] - 48) * 100 + (data[3] - 48) * 10 + (data[4] - 48)# = #int(data.encode('hex'),16)
 
-            framelength = final_data_int#int(data)#final_data_int/8  # xx bytes
-            # Read the current frame
-            frame = self.file.read(framelength)
-            if len(frame) != framelength:
-                raise ValueError('incomplete frame data')
-            #print "frame length"
-            #print len(frame)
-            #if not (data.startswith(b'\xff\xd8') and data.endswith(b'\xff\xd9')):
-            #    raise ValueError('invalid jpeg')
+                final_data_int = data_int
+                framelength = final_data_int  #int(data)#final_data_int/8  # xx bytes
+                # Read the current frame
+                frame = self.file.read(framelength)
+                if len(frame) != framelength:
+                    raise ValueError('incomplete frame data')
+                #print "frame length"
+                #print len(frame)
+                #if not (data.startswith(b'\xff\xd8') and data.endswith(b'\xff\xd9')):
+                #    raise ValueError('invalid jpeg')
 
-            self.frameNum += 1
-            print('-'*10 + "\nNext Frame (#" + str(self.frameNum) + ") length:" + str(framelength) + "\n" + '-'*10)
+                self.frameNum += 1
+                print('-'*10 + "\nNext Frame (#" + str(self.frameNum) + ") length:" + str(framelength) + "\n" + '-'*10)
 
-            return frame
+                return frame
 
     def frameNbr(self):
         """Get frame number."""
